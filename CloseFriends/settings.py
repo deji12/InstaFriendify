6659dc81bf271007011d2f1e3cb06ed6d7fb8856
deji12 <adesolaayodeji53@gmail.com>
2025-03-15 13:38:24 +0100

    removed staticfiles

--- CloseFriends/settings.py
+++ CloseFriends/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompresedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompresedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
