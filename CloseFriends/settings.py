94dd4eb34ae619798508c10a4d6680469a4c95b4
deji12 <adesolaayodeji53@gmail.com>
2025-03-20 20:51:45 +0100

    added 404 and 500 pages

--- CloseFriends/settings.py
+++ CloseFriends/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://amicipiustretti.com', 'amicipiustretti.com']
+CSRF_TRUSTED_ORIGINS = ['https://amicipiustretti.com']
 
 
 # Application definition
